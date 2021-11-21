93bf67a479c4a4878a801ae115f38d0161c667d5
mohammed abdullah <Mohammed.Alhethely.has.s@tuwaiq.edu.sa>
2021-11-21 15:25:10 +0300

    Done The HW SERVICES

---
+++ SERVICES/SERVICES/Pet.swift
@@ -0,0 +1,8 @@
+//
+//  Pet.swift
+//  SERVICES
+//
+//  Created by Mohammed Abdullah on 16/04/1443 AH.
+//
+
+import Foundation
---
+++ SERVICES/SERVICES/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SERVICES
+//
+//  Created by Mohammed Abdullah on 15/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

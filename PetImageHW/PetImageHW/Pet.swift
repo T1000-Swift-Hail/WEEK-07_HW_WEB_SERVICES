e7a0d82f23180cfff1fc40ad4b537a2d05292068
Hind Alharbi <Hind.Alharbi.has.s@tuwaiq.edu.sa>
2021-11-21 14:17:39 +0300

    DONE_HW_WEB_SERVICES

---
+++ PetImageHW/PetImageHW/Pet.swift
@@ -0,0 +1,14 @@
+//
+//  Pet.swift
+//  PetImageHW
+//
+//  Created by Hind Alharbi on 11/21/21.
+//
+
+import Foundation
+struct Pet : Decodable{
+    let message: String
+    let status: String
+}
+
+

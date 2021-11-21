c8d1bcddb4a17ba05ee8938445e45e33e927d549
Mastorhalzamil <mastorh.dogaim.has.s@tuwaiq.edu.sa>
2021-11-21 14:23:05 +0300

    donehw

---
+++ RandomPetImageHw/Pet.swift
@@ -0,0 +1,14 @@
+//
+//  Pet.swift
+//  RandomPetImageHw
+//
+//  Created by Mastorah on 21/11/2021.
+//
+
+import Foundation
+
+struct Pet: Decodable {
+    let message : String
+    let status: String
+    
+}

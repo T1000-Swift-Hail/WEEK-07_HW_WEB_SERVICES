f652ea1ba5888963fbe85a639a929037aaeff893
AsmaRashed1 <Asma.Rashed.has.s@tuwaiq.edu.sa>
2021-11-21 14:06:51 +0300

    Done

---
+++ Random/Random/ViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ViewController.swift
+//  Random
+//
+//  Created by Asma Rasheed on 16/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let urlSession : URLSession = URLSession(configuration: .default)
+
+    @IBOutlet weak var petImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    
+    func getPetDataFromWeb() {
+        
+      guard let apiURL = URL(string: "https://dog.ceo/api/breeds/image/random") else {
+      print("you have a wrong url")
+      return
+        }
+        
+     let urlRequest = URLRequest(url: apiURL)
+        
+     let dataTask = urlSession.dataTask(with: urlRequest, completionHandler: { data, response, error in
+            
+     if let error = error {
+     print(error)
+     }
+            
+      if let data = data {
+
+       do {
+       let newPet : Pet = try JSONDecoder().decode(Pet.self, from: data)
+      print(newPet)
+      self.getPetImageFromWebDownloadTask(pet: newPet)
+      } catch {
+      print(error)
+                }
+            }
+            
+        })
+        
+        
+        dataTask.resume()
+    }
+    
+    
+    func getPetImageFromWeb(pet : Pet) {
+        
+        guard let imageURL = URL(string: pet.message) else {
+            
+            print("image url is wrong. Check it out")
+            return
+        }
+        
+        
+        do {
+            let imageData =  try Data(contentsOf: imageURL)
+            let petImage = UIImage(data: imageData)
+            
+            DispatchQueue.main.async {
+                self.petImageView.image = petImage
+            }
+            
+            
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    
+    func getPetImageFromWebDownloadTask (pet : Pet) {
+        
+        guard let imageURL = URL(string: pet.message) else {
+            return
+        }
+        
+        
+        let downloadTask = urlSession.downloadTask(with: imageURL, completionHandler: { localURL , response, error in
+            
+            if let error = error {
+                print(error)
+            }
+            
+            if let localURL = localURL {
+                
+                do {
+                  let imageDataFromTempFile = try Data(contentsOf: localURL)
+                  let petImage = UIImage(data: imageDataFromTempFile)
+                  
+                    DispatchQueue.main.async {
+                      self.petImageView.image = petImage
+                    }
+                    
+                    
+                } catch {
+                    print(error)
+                }
+            }
+            
+            
+            
+        })
+        
+        
+        downloadTask.resume()
+        
+    }
+
+    
+    @IBAction func showImageView(_ sender: Any) {
+   
+    
+        getPetDataFromWeb()
+    }
+    
+}
+
+
+

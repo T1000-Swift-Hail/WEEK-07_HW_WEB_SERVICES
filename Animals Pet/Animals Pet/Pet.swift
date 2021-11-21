5d83a6d470a6b73aa142bbd3a18a14b63b668b2d
Aryaf AlQabali <Aryaf.Aqeel.has.s@tuwaiq.edu.sa>
2021-11-21 14:14:43 +0300

    Done

---
+++ Animals Pet/Animals Pet/Pet.swift
@@ -0,0 +1,13 @@
+//
+//  Pet.swift
+//  Animals Pet
+//
+//  Created by AryafAlaqabali on 16/04/1443 AH.
+//
+
+import Foundation
+
+struct Pet : Decodable {
+    let message : String
+    let status : String
+}
